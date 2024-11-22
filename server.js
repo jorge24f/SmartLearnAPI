8d42b2511a6c842916f739172c5d87968a7ae0dd
jorge24f <jflg240104@gmail.com>
2024-11-21 20:25:14 -0600

    Agregando endpoints

--- server.js
+++ server.js
@@ -41,13 +41,13 @@ app.listen(port, () => {
     run();
 }); 
 
-/* sign up */
+/* signUp */
 app.post('/signUp', async (req, res)=>{
     try{
         const cliente = new MongoClient(uri);
-        //conectarse a la db
+     
         const database = cliente.db('SmartLearn');
-        //seleccionar la coleccion
+       
         const collection = database.collection('Users');
 
         const collection2 = database.collection("Institutions");
@@ -90,7 +90,7 @@ app.post('/signUp', async (req, res)=>{
                 userName: req.body.userName,
                 mail: req.body.mail,
                 password: req.body.password,
-                institutionID: req.body.institutionID,
+                institutionID: new ObjectId(req.body.institutionID),
                 role: req.body.role,
                 registerDate: new Date()
             });
@@ -109,7 +109,7 @@ app.post('/signUp', async (req, res)=>{
     }
 });
 
-/* login */
+/* logIn */
 app.post('/login', async (req, res) => {
     try {
         const cliente = new MongoClient(uri);
@@ -119,7 +119,7 @@ app.post('/login', async (req, res) => {
         const collection = database.collection('Users');
         // Buscar el usuario en la base de datos
         const user = await collection.findOne({
-            mail: req.body.mail,
+            userName: req.body.userName,
             password: req.body.password
         });
 
@@ -128,11 +128,7 @@ app.post('/login', async (req, res) => {
             console.log("Login exitoso");
             res.status(200).send({
                 message: "Login exitoso",
-                usuario: {
-                    id: user._id,
-                    mail: user.mail,
-                    // Puedes devolver otros datos si los necesitas
-                }
+                user: user
             });
         } else {
             // Usuario no encontrado
@@ -150,15 +146,15 @@ app.post('/login', async (req, res) => {
     }
 });
 
-/* Create Institution */
-app.post('/CreateInstitution', async (req, res)=>{
+/* createInstitution */
+app.post('/CreateInstitution', async (req, res)=>{s
     try{
         const cliente = new MongoClient(uri);
-        //conectarse a la db
+        
         const database = cliente.db('SmartLearn');
-        //seleccionar la coleccion
+        
         const collection = database.collection('Institutions');
-        //insertar un documento
+        
         const resultado = await collection.insertOne({
             name: req.body.name,
             address: req.body.address,
@@ -174,6 +170,134 @@ app.post('/CreateInstitution', async (req, res)=>{
         console.log("No se pudo crear la institucion", error);
         res.status(500).send({
             message: "No se pudo crear la institucion"+error
+        });
+    }
+});
+
+/* createCourse */
+app.post('/createCourse', async (req, res)=>{
+    try{
+        const cliente = new MongoClient(uri);
+
+        const database = cliente.db('SmartLearn');
+
+        const collection = database.collection('Courses');
+
+        const resultado = await collection.insertOne({
+            institutionID: new ObjectId(req.body.institutionID),
+            user_id: new ObjectId(req.body.user_id),
+            name: req.body.name,
+            description: req.body.description,
+            creationDate: new Date()
+        });
+        console.log(resultado);
+        console.log('Curso creado con exito');
+        res.status(200).send({
+            message: "Curso creado con exito", 
+            resultado: resultado
+        });    
+    }catch(error){
+        console.log("No se pudo crear el curso", error);
+        res.status(500).send({
+            message: "No se pudo crear el curso"+error
+        });
+    }
+});
+
+/* getCursosCreados */ 
+app.get('/getCursosCreados', async (req,res)=>{
+    try{
+        const cliente = new MongoClient(uri);
+        
+        const database = cliente.db('SmartLearn');
+        
+        const collection = database.collection('Courses');
+
+        const findResult = await collection.find({
+            user_id: new ObjectId(req.query.user_id)
+        }).toArray();
+
+        if(findResult.length > 0){
+            console.log(findResult);
+            res.status(200).send({
+                message: 'Informacion obtenida con exito',
+                resultado: findResult
+            });
+        } else{
+            console.log('No se encontraron cursos para este usuario');
+            res.status(404).send({
+                message: 'No se encontraron cursos para este usuario',
+                resultado: []
+            });
+        }
+    }catch(error){
+        console.log('Ocurrio un error', error);
+        res.status(500).send({
+            message: "algo salio mal",
+            resultado: []
+        });
+    }
+});
+
+/* getCursosAsignados */ 
+app.get('/getCursosAsignados', async (req,res)=>{
+    try{
+        const cliente = new MongoClient(uri);
+        
+        const database = cliente.db('SmartLearn');
+        
+        const collection = database.collection('Assigned_Courses');
+
+        const findResult = await collection.find({
+            user_id: new ObjectId(req.query.user_id)
+        }).toArray();
+
+        if(findResult.length > 0){
+            console.log(findResult);
+            res.status(200).send({
+                message: 'Informacion obtenida con exito',
+                resultado: findResult
+            });
+        } else{
+            console.log('No se encontraron cursos para este usuario');
+            res.status(404).send({
+                message: 'No se encontraron cursos para este usuario',
+                resultado: []
+            });
+        }
+    }catch(error){
+        console.log('Ocurrio un error', error);
+        res.status(500).send({
+            message: "algo salio mal",
+            resultado: []
+        });
+    }
+});
+
+/* assignCourse */
+app.post('/assignCourse', async (req, res)=>{
+    try{
+        const cliente = new MongoClient(uri);
+        
+        const database = cliente.db('SmartLearn');
+        
+        const collection = database.collection('Assigned_Courses');
+
+         const resultado = await collection.insertOne({
+            user_id: new ObjectId(req.body.user_id),
+            course_id: new ObjectId(req.body.course_id),
+            completion: req.body.completion
+        });
+        console.log(resultado);
+        console.log('Curso asignado con exito!');
+        res.status(200).send({
+            message: "Curso asignado con exito!", 
+            resultado: resultado
+        });   
+    }catch(error){
+        console.log("No se pudo asignar el curso!", error);
+        res.status(500).send({
+            message: "No se pudo asignar el curso!"+error
         });
     }
 });
